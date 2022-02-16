a6f5eb7611313fe375b484263475d0d117e511aa
Ralphadaba <ralphadab@gmail.com>
2022-02-16 19:32:11 +0100

    Second save

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getHome(): {} {
+    return { title: 'Home Page' };
   }
+
+  getAboutUs(): {} {
+    return { title: 'About Us Page' };
+  }
+
+  getHello(): string {
+    return 'Hello World from ISMS2020!';
+  }
+
+  getHello2(): {} {
+    return { message: 'Hello World!', title: 'My Greeting App' };
+  }
+
 }
